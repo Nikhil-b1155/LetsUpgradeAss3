c47707a573f926ad3e60b76a2920af980242e396
Nikhil-b1155 <72803852+Nikhil-b1155@users.noreply.github.com>
2020-10-14 20:24:56 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,58 +1,40 @@
-
-package com.employee;
+package com.student;
 import java.util.Scanner;
-public class Employee {
-    public static void main(String[] args) {
-        String employee_name;
-        int DOB,MOB,YOB;
-        int monthly_salary;
-        int age;
-        int yearly_salary;
-        double tax;
+public class Student {
+    public static void main(String[] args){
+        int hindi,english,maths,science,social;
+        double percent;
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter your name");
-        employee_name=s.next();
-        System.out.println("Enter your date of birth");
-        DOB=s.nextInt();
-        System.out.println("Enter your month of birth");
-        MOB=s.nextInt();
-        System.out.println("Enter your year of birth");
-        YOB=s.nextInt();
-        System.out.println("Enter your monthly salary");
-        monthly_salary = s.nextInt();
-        System.out.println("Your name is "+employee_name);
-        age=ageCalculator(YOB);
-        if(YOB>0){
-            System.out.println("Your age is "+age);
+        System.out.println("Enter hindi marks");
+        hindi=s.nextInt();
+        System.out.println("Enter english marks");
+        english=s.nextInt();
+        System.out.println("Enter maths marks");
+        maths=s.nextInt();
+        System.out.println("Enter science marks");
+        science=s.nextInt();
+        System.out.println("Enter social marks");
+        social=s.nextInt();
+        int sum = hindi+english+maths+science+social;
+        percent = (sum/500.0)*100;
+//        System.out.println(sum);
+        if(percent>90 && percent<=100){
+            System.out.println("Grade is A+ and percent is "+percent);
         }
-        yearly_salary=monthly_salary*12;
-        System.out.println("Your annual salary is "+yearly_salary);
-
-        if(yearly_salary>=500000 && yearly_salary<=600000){
-            tax=(28/100.0)*yearly_salary;
+        else if (percent>80 && percent<=90){
+            System.out.println("Grade is A and percent is "+percent);
         }
-        else if(yearly_salary>=400000 && yearly_salary<500000 ){
-            tax=(15/100.0)*yearly_salary;
+        else if (percent>70 && percent<=80){
+            System.out.println("Grade is B and percent is "+percent);
         }
-        else if(yearly_salary>=300000 && yearly_salary<400000){
-            tax=(10/100.0)*yearly_salary;
+        else if (percent>60 && percent<=70){
+            System.out.println("Grade is C and percent is "+percent);
         }
-        else if (yearly_salary>=200000 && yearly_salary<300000){
-            tax=(5/100.0)*yearly_salary;
+        else if (percent>50 && percent<=60){
+            System.out.println("Grade is E and percent is "+percent);
         }
         else{
-            tax=0;
-        }
-        System.out.println("The tax amount applicable is "+(int)tax);
-    }
-    public static int ageCalculator(int year){
-        int presentYear=2020;
-        if(presentYear==year){
-            System.out.println("Invalid age");
-            return 0;
-        }
-        else {
-            return presentYear-year;
+            System.out.println("Grade is F and percent is "+percent);
         }
     }
 }
